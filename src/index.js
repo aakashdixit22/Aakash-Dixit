64c3d117712c0b3ea2de6c1131fd9378873f3420
aakashdixit22 <aakashdixit227@gmail.com>
2025-03-22 17:36:03 +0530

    Add Vercel Analytics integration and update dependencies

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ThemeContextProvider from './contexts/ThemeContext'
-
+import { Analytics } from "@vercel/analytics/react"
 
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
@@ -12,6 +12,7 @@ import "slick-carousel/slick/slick-theme.css";
 ReactDOM.render(
     <ThemeContextProvider>
       <App />
+      <Analytics />
     </ThemeContextProvider>,
   document.getElementById('root')
 );
